fdb5bcb266a54ab7ea0a1165b903e99ba870f50a
Vivek-Mishra-IIT <vivekmishraiit@gmail.com>
2019-01-13 06:15:42 +0530

    Uploading App

---
+++ EnComm2/app/src/main/java/com/example/teamdevhackers/encomm/Complaints/InvestorsActivity.java
@@ -0,0 +1,15 @@
+package com.example.teamdevhackers.encomm.Complaints;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.teamdevhackers.encomm.R;
+
+
+public class InvestorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investors);
+    }
+}
---
+++ EnComm2/app/src/main/java/com/example/teamdevhackers/encomm/Mess/EntreActivity.java
@@ -0,0 +1,14 @@
+package com.example.teamdevhackers.encomm.Mess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.teamdevhackers.encomm.R;
+
+public class EntreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entre);
+    }
+}
